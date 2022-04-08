8bb1d0f0ba0ea2ed81cc03961ff306876474c934
jk <54949016+jeyakeerthanan@users.noreply.github.com>
2022-04-08 22:02:22 +0530

    Signed-off-by: jk <54949016+jeyakeerthanan@users.noreply.github.com>

---
+++ src/test/java/com/moodle/MoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
